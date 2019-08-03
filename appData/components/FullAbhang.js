35fde045fb209ad43a93e36896fac32366458830
Sudarshana Patil <patilsudarshana94@gmail.com>
2019-08-03 11:52:45 +0530

    added font changer button

--- appData/components/FullAbhang.js
+++ appData/components/FullAbhang.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import { Text, View, Dimensions, ScrollView, StyleSheet,ImageBackground } from 'react-native';
+import { Text, View, Dimensions, ScrollView, StyleSheet, ImageBackground } from 'react-native';
 const { width, height } = Dimensions.get('window');
-
+let style = StyleSheet.create({
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover'
+    }
+})
 export default class FullAbhang extends Component {
     render() {
         return (
-            <View style={{
-                width: width, height: height,
+            <ImageBackground style={{width:width-20,height:height-20,margin:10}} source={require('../../images/1.jpg')}>
+
+            {/* <View style={{
+                width, height,
                 backgroundColor: 'antiquewhite', alignContent: 'center'
-            }}>
-            <View style={{margin:5,padding:10}}>
-            <ImageBackground source={require('../../images/1.jpg')}>
-                <Text style={{
-                    alignContent: 'center', alignItems: 'center', textAlign: "justify",
-                    alignSelf: 'center', fontWeight: 'bold', fontSize: 20
-                }}>
-                    समचरणदृष्टि विटेवरी साजिरी । तेथें माझी हरी वृत्ति राहो ॥१॥{"\n"}
-                    आणीक न लगे मायिक पदार्थ । तेथें माझें आर्त्त नको देवा ॥ध्रु.॥{"\n"}
-                    ब्रम्हादिक पदें दुःखाची शिराणी । तेथें दुश्चित झणी जडों देसी ॥२॥{"\n"}
-                    तुका म्हणे त्याचें कळलें आम्हां वर्म । जे जे कर्मधर्म नाशवंत ॥३॥{"\n"}
+            }}> */}
+                <View style={{ margin: 5, padding: 10 }}>
+                    <Text style={{
+                        alignContent: 'center', alignItems: 'center', textAlign: "justify",
+                        alignSelf: 'center', fontWeight: 'bold', fontSize: 20
+                    }}>
+                        समचरणदृष्टि विटेवरी साजिरी । तेथें माझी हरी वृत्ति राहो ॥१॥{"\n"}
+                        आणीक न लगे मायिक पदार्थ । तेथें माझें आर्त्त नको देवा ॥ध्रु.॥{"\n"}
+                        ब्रम्हादिक पदें दुःखाची शिराणी । तेथें दुश्चित झणी जडों देसी ॥२॥{"\n"}
+                        तुका म्हणे त्याचें कळलें आम्हां वर्म । जे जे कर्मधर्म नाशवंत ॥३॥{"\n"}
                     </Text>
-                    </ImageBackground>
-                    </View>
-            </View>)
+                   
+                </View>
+            {/* </View> */}
+            </ImageBackground>)
     }
 }
